71fd34899e548fd8715d6bafc2458f23f152ed41
cap153 <67049883+cap153@users.noreply.github.com>
2024-05-31 21:42:23 +0800

    Update gd_killer.py
    1. 在到达秒杀时间时如果出现问题会进行异常捕捉，可以手动继续后续步骤
    2. 测试代码时发现间隔1分钟刷新页面会出现503，添加判断机制实现反复刷新直到页面正常加载
    3. 添加抢购成功的提示信息打印

--- gd_killer.py
+++ gd_killer.py
@@ -2,19 +2,17 @@ from DrissionPage import ChromiumPage
 import datetime
 
 # 创建对象
-page = ChromiumPage(9223)
+page = ChromiumPage(9224)
 
 # 指定秒杀时间
 kill_time = "2024-05-31 21:25:00.00000000"
 
-# 打开淘宝网页
-page.get("https://www.taobao.com")
+# 打开京东网页
+page.get("https://www.jd.com")
 # 点击购物车
-page.ele('x://*[@id="J_MiniCart"]/div[1]/a/span[2]').click()
+cart = page.ele('x://*[@id="settleup"]/div[1]/a').click.for_new_tab()
 # 等待登录完成，直到购物车全选按钮出现，超时时间我设置为1分钟
-page.wait.ele_displayed('x://*[@id="J_SelectAll1"]/div/label',timeout=60)
-# 点击购物车全选按钮
-page.ele('x://*[@id="J_SelectAll1"]/div/label').click()
+cart.wait.ele_displayed('x://*[@id="cart-body"]/div[2]/div[4]/div[1]/div/input',timeout=60)
 
 while(True):
     # 获取当前时间
@@ -23,31 +21,35 @@ while(True):
     # 判断当前时间是否到达了秒杀时间
     if(now>kill_time):
         try:
+            # 判断商品是否全选(京东购物车会记住上次选择的商品)
+            if cart.ele('x://*[@id="cart-body"]/div[2]/div[4]/div[1]/div/input').attr('clstag').split('|')[-1].startswith('0'):
+                # 没有全选的情况，点击购物车全选按钮
+                cart.ele('x://*[@id="cart-body"]/div[2]/div[4]/div[1]/div/input').click()
             # 点击结算按钮
-            page.ele('x://*[@id="J_Go"]/span').click()
-            # 下单商品
-            page.ele('提交订单').click()
+            cart.ele('去结算').click()
+            # 点击提交订单
+            cart.ele('x://*[@id="order-submit"]/b').click()
+            # 点击立即支付
+            cart.ele('x://*[@id="indexBlurId"]/div[2]/div[1]/div[2]/div/div[2]/div[2]/div[2]/div/div/div[1]').click()
             # 自动填充密码(需要修改成你自己的支付密码)
-            # page.ele('x://*[@id="payPassword_rsainput"]').input("123456")
-            # 确定
-            # page.ele('x://*[@id="validateButton"]').click()
+            # cart.ele('x://*[@id="validateShortFake"]').input('123456')
+            # 点击立即支付
+            # cart.ele('x://*[@id="baseMode"]/div/div[2]/div/div[2]/div/div/div[1]').click()
             break
         except Exception as err:
             # 如果发生任何异常都进行捕捉，防止浏览器退出
             print("%s\n发生了错误，请手动完成后续步骤"%err+input())
-    # 判断当前秒数是不是0，实现间隔一分钟刷新页面，防止掉登录
+    # 判断当前秒数是不是0，实现间隔一分钟刷新页面，防止掉登录(京东购物车会记住上次选择的商品)
     if(datetime.datetime.now().second == 0):
-        page.refresh() # DrissionPage的页面刷新方法，内置了wait.load_start()程序会自动等待加载结束
+        cart.refresh() # DrissionPage的页面刷新方法，内置了wait.load_start()程序会自动等待加载结束
         while(True):
             try:
                 # 等待全选按钮加载
-                page.wait.ele_displayed('x://*[@id="J_SelectAll1"]/div/label')
+                cart.wait.ele_displayed('x://*[@id="cart-body"]/div[2]/div[4]/div[1]/div/input')
                 break
             except:
                 # 没有成功加载按钮说明出现了错误，无论什么错误都再次刷新页面
-                page.refresh() # DrissionPage的页面刷新方法，内置了wait.load_start()程序会自动等待加载结束
-        # 再次全选购物车
-        page.ele('x://*[@id="J_SelectAll1"]/div/label').click()
+                cart.refresh() # DrissionPage的页面刷新方法，内置了wait.load_start()程序会自动等待加载结束
 
 # 成功的信息输出和测试时的程序暂停
 input('恭喜，抢购成功')
